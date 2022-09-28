41e1cd0dde40ce5ad9a87df9e061ec265d37d9cd
Surya Darma <budisuryadarma0407@gmail.com>
2022-09-28 20:02:04 +0800

    init package

---
+++ src/injectGSIScript.ts
@@ -0,0 +1,38 @@
+import {CredentialResponse} from 'google-one-tap'
+
+export const injectGSIScript = (
+  onUserSignIn: (credentialResponse: CredentialResponse) => Promise<void>
+) => {
+  if (document && typeof google === 'undefined') {
+    const script = document.createElement('script')
+    script.src = 'https://accounts.google.com/gsi/client'
+    script.onload = () => {
+      google.accounts.id.initialize({
+        client_id: config.googleClientID,
+        callback: onUserSignIn,
+      })
+      google.accounts.id.prompt((notification) => {
+        const momentType = notification.getMomentType()
+        const isDisplayed = notification.isDisplayed()
+        const dismissedReason = notification.getDismissedReason()
+        // if (isDisplayed && momentType === 'display') {
+        //   impression('view')
+        // }
+        const isUserTapOutside = !isDisplayed && momentType === 'skipped'
+        const isUserCloseByNavigate =
+          momentType === 'dismissed' &&
+          !isDisplayed &&
+          dismissedReason === 'cancel_called'
+        // if (isUserTapOutside || isUserCloseByNavigate) {
+        //   impression('close')
+        // }
+      })
+    }
+    script.async = true
+    script.defer = true
+    const body = document.querySelector('body')
+    if (body) {
+      body.appendChild(script)
+    }
+  }
+}
